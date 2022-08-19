e14ac4550ebd4b1ca8f693bf16f55abdc1361a43
Peter Schuelke <pschuelke@phase2technology.com>
2022-08-19 10:20:33 -0700

    feat(init): Outline installation.

---
+++ src/.storybook/CustomTheme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Dev Tell Demo',
+  fontBase: 'Inter var, Helvetica, Arial, sans-serif',
+});
